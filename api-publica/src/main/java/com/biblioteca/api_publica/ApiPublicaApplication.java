b12ce3863fb6a0841bb2bb7f5e654e8de77024ae
Matheus Novo <143052348+matheus-novo@users.noreply.github.com>
2026-04-06 20:26:18 +0000

    first commit

---
+++ api-publica/src/main/java/com/biblioteca/api_publica/ApiPublicaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.api_publica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPublicaApplication.class, args);
+	}
+
+}
---
+++ api-publica/src/test/java/com/biblioteca/api_publica/ApiPublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.api_publica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
